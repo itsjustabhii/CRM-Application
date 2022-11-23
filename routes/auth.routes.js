ab190ff090ecd347c6f2fcb8ed2df621872387ab
Abhishek <itsjustabhii.com>
2022-11-23 14:45:08 +0530

    auth routes added

---
+++ routes/auth.routes.js
@@ -0,0 +1,9 @@
+const authController = require('../controller/auth.controller')
+const verifySignup = require('../middlewares/signup.middleware')
+
+module.exports = function(app) {
+    //verifySignup.validateSignupRequest
+    app.post("/crm/api/v1/auth/signup", authController.signup)
+
+    app.post("/crm/api/v1/auth/signin", authController.signin)
+}
