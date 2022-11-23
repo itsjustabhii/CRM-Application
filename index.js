4fa32c159871f28a265866a4fc758b2372ce7eaa
Abhishek <itsjustabhii.com>
2022-11-23 14:44:23 +0530

    modified mongoose connect

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express') 
 const config = require('./config/config')
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -9,7 +10,17 @@ app.get('/', (req, res) => {
     res.sendStatus(200)
 })
 
+
 //Starting the server
-app.listen(config.port, () => {
-    console.log(`Listening on port ${config.port}`);
+mongoose.connect('mongodb://localhost/demodb', {family:4}, (err) => {
+    if(err) {
+        console.log('Error occurred');
+    }
+    else {
+        console.log('Connected');
+        app.listen(port, () => {
+            console.log(`Example app listening at port http://localhost:${port}`);
+        })
+    }
 })
+
